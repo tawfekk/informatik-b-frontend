ef397ede5de1f0bab8d081dd7d17f68a8d9b4ac1
Tawfekk <a@tawfek.dk>
2024-02-27 18:50:58 +0100

    FIxes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
 import {
     Admin,
     Resource,
-    ListGuesser,
-    EditGuesser,
-    ShowGuesser,
     defaultLightTheme,
     CustomRoutes,
     defaultDarkTheme,
@@ -58,36 +55,5 @@ export const App = () => (
         </Admin>
     </BrowserRouter>
 );
-
-
-/*
-function App() {
-    return (
-        <Admin
-            dataProvider={dataProvider}
-            lightTheme={defaultLightTheme}
-            darkTheme={defaultDarkTheme}
-        >
-            <Resource
-                name="posts"
-                list={PostList}
-                edit={PostEdit}
-                create={PostCreate}
-                show={ShowGuesser}
-                recordRepresentation="title"
-                icon={BookIcon}
-            />
-            <Resource
-                name="comments"
-                list={ListGuesser}
-                edit={EditGuesser}
-                show={ShowGuesser}
-                icon={ChatBubbleIcon}
-            />
-            <Resource name="tags" recordRepresentation={tag => tag.name.en} />
-        </Admin>
-    );
-}
-*/
 
 export default App;
